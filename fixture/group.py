ab8e1214f304b27da30b1e6b8e40a8af7f68ecc1
posypaylo_cr <posypaylo.cr@gmail.com>
2017-04-03 10:09:23 +0300

    New commit

---
+++ fixture/group.py
@@ -0,0 +1,65 @@
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text.name is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_first_group()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_first_group()
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
