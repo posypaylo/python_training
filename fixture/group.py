dc3ef4abb88df8b8bde6fc5770e0f38d350ba1e5
Artem Posypaylo <posypaylo2@gmail.com>
2017-04-04 00:22:14 +0300

    Все сломалось

--- fixture/group.py
+++ fixture/group.py
@@ -7,11 +7,13 @@ class GroupHelper:
 
     def open_group_page(self):
         wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            return
         wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
-        if text.name is not None:
+        if text is not None:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
@@ -65,4 +67,4 @@ class GroupHelper:
     def count(self):
         wd = self.app.wd
         self.open_group_page()
-        return len(wd.find_elements_by_name("selected[]").click())
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,11 +1,7 @@
 from model.group import Group
 
-def test_add_group_name(app):
-    app.session.login(username="admin", password="secret")
+def test_modify_group_name(app):
     app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
 
-def test_add_group_header(app):
-    app.session.login(username="admin", password="secret")
+def test_modify_group_header(app):
     app.group.modify_first_group(Group(header="New header"))
-    app.session.logout()
