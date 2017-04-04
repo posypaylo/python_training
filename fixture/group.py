3d29c6f9dbf1be5c2df9d3a29742b1331dfdf1fa
posypaylo_cr <posypaylo.cr@gmail.com>
2017-04-04 12:37:30 +0300

    все починил

--- fixture/group.py
+++ fixture/group.py
@@ -8,8 +8,7 @@ class GroupHelper:
     def open_group_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
-            return
-        wd.find_element_by_link_text("groups").click()
+            wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
