522dbb3d19ca8f55027b136548ba35d32fd6dd58
posypaylo_cr <posypaylo.cr@gmail.com>
2017-04-05 18:42:13 +0300

    Geometry

---
+++ geometry.py
@@ -0,0 +1,24 @@
+
+from math import sqrt
+
+
+class Point:
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
+
+    def distance(self, p2):
+        dx = p2.x - self.x
+        dy = p2.y - self.y
+        return sqrt(dx*dx + dy*dy)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+# print(Point(0, 0).distance(Point(3, 4)))
+a = Point(0, 0)
+b = Point(3, 4)
+print(a.distance(b))
+print(a == b)
+print(a == Point(0, 0))
