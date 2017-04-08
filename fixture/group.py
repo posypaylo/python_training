6023e9e0af149a72fe4ca7a208fa945448facc33
posypaylo_cr <posypaylo.cr@gmail.com>
2017-04-08 18:10:50 +0300

    4.2 Сравнение списков

--- fixture/group.py
+++ fixture/group.py
@@ -1,11 +1,11 @@
-
+from model.group import Group
 
 class GroupHelper:
 
     def __init__(self, app):
         self.app = app
 
-    def open_group_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
@@ -26,7 +26,7 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
-        self.open_group_page()
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
@@ -41,7 +41,7 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
-        self.open_group_page()
+        self.open_groups_page()
         self.select_first_group()
         #submit deletion
         wd.find_element_by_name("delete").click()
@@ -49,7 +49,7 @@ class GroupHelper:
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
-        self.open_group_page()
+        self.open_groups_page()
         self.select_first_group()
         # open modification form
         wd.find_element_by_name("edit").click()
@@ -65,5 +65,15 @@ class GroupHelper:
 
     def count(self):
         wd = self.app.wd
-        self.open_group_page()
+        self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
