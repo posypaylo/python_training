5c50aa7f45a7f11bef025aa31c0a45376f0dccf5
posypaylo_cr <posypaylo.cr@gmail.com>
2017-04-11 18:35:45 +0300

    4.3 сортировка списков

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,5 +7,5 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
-    old_groups[0:1] = [] # удаление элементов 0:1 (1 элемент с индексом 0)
+    old_groups[0:1] = [] # удаление элементов 0:1 (один элемент с индексом 0)
     assert old_groups == new_groups
