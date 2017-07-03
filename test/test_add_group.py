b42864ca2c3153beb2661abb8f0b0d3d94ca9e9b
posypaylo_cr <posypaylo.cr@gmail.com>
2017-07-03 12:40:56 +0300

    6.3 ddt

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-    for i in range(5)
-]
-
-# testdata = [
-#     Group(name=name, header=header, footer=footer)
-#     for name in ["", random_string("name", 10)]
-#     for header in ["", random_string("header", 20)]
-#     for footer in ["", random_string("footer", 20)]
-# ]
-
-
+# from data.add_group import testdata
+from data.add_group import constant as testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
-    # check1 = int(len(old_groups) + 1)
-    # check2 = int(app.group.count())
-    # print(check1)
-    # print(check2)
-    # assert check1 == check2
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
