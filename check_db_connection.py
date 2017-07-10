c61cf1a2c2897709116f1516ff1c86d9534f34ad
posypaylo_cr <posypaylo.cr@gmail.com>
2017-07-10 18:42:56 +0300

    7.4 assertions

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,40 @@
-import mysql.connector
+from fixture.orm import ORMFixture
 
-
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="root")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="root")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for raw in cursor.fetchall():
-        print(raw)
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass   #  db.destroy()
+
+
+# import mysql.connector
+#
+#
+# connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="root")
+#
+# try:
+#     cursor = connection.cursor()
+#     cursor.execute("select * from group_list")
+#     for raw in cursor.fetchall():
+#         print(raw)
+# finally:
+#     connection.close()
+
+
+# from fixture.db import DbFixture
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="root")
+#
+# try:
+#     groups = db.get_group_list()
+#     for group in groups:
+#         print(group)
+#     print(len(groups))
+# finally:
+#     db.destroy()
+
+
